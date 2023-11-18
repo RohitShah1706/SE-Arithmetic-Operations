d3a5291ca60a74b69412d5c6971758d4894ad4ee
RohitShah1706 <rlshah03@gmail.com>
2023-11-18 10:30:36 +0530

    removed unused imports from main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,7 @@
-// #include "src/Digit/Digit.h"
-// #include "src/Arithmetic/Add/AddArithmetic.h"
-// #include "src/Operators/Add/AddOperator.h"
 #include "src/GUI/GUI.h"
 
 int main()
 {
-//     string s1, s2;
-//     cin >> s1 >> s2;
-
-//     Digit a(s1, 10);
-//     Digit b(s2, 10);
-
-//     AddArithmetic addArithmetic = AddArithmetic();
-//     AddOperator addOperator(&addArithmetic);
-
-//     a.display();
-//     b.display();
-
-//     Digit c = addOperator.operate(a, b);
-//     c.display();
     GUI::run();
     return 0;
 }
